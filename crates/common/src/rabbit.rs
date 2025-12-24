b19cc631314a08181b0c9cbcaa426616bff7ff3e
Александр Сергиенко <s_sandr06@mail.ru>
2025-12-24 22:07:21 +0300

    Добавлены константы (для RabbitMQ и названия таблиц)

---
+++ crates/common/src/rabbit.rs
@@ -0,0 +1,4 @@
+// Константы для RabbitMQ (Exchange и Routing Keys)
+pub const EXCHANGE_ORDER: &str = "order_exchange";
+pub const ROUTING_KEY_ORDER_CREATED: &str = "order.created";
+pub const ROUTING_KEY_ORDER_PAID: &str = "order.paid";
---
+++ crates/common/src/tables.rs
@@ -0,0 +1,5 @@
+// Константы имен таблиц в БД
+pub const TABLE_ACCOUNTS: &str = "accounts";
+pub const TABLE_ORDERS: &str = "orders";
+pub const TABLE_OUTBOX: &str = "outbox";
+pub const TABLE_INBOX: &str = "inbox";
