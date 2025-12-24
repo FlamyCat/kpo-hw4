77b4475ff15dc8f3dbbabe2abb09ea653a636a68
Александр Сергиенко <s_sandr06@mail.ru>
2025-12-24 23:24:20 +0300

    Добавлена функция подключения к БД

--- crates/common/src/db_utils.rs
+++ crates/common/src/db_utils.rs
@@ -1,5 +1,11 @@
 use serde::{Deserialize, Serialize};
-use surrealdb::sql::Thing;
+use std::env;
+use surrealdb::{
+    engine::remote::ws::{Client, Ws},
+    opt::auth::Root,
+    sql::Thing,
+    Surreal,
+};
 
 /// Структура записи в таблице Outbox
 #[derive(Debug, Serialize, Deserialize)]
@@ -11,4 +17,34 @@ pub struct OutboxRecord {
     pub routing_key: String,
     pub created_at: String,
     pub processed: bool,
+}
+
+/// Подключиться к СУБД
+///
+/// Автоматически определяет адрес подключения:
+/// * Если задана переменная окружения `IS_DOCKER=true`, используется "surrealdb:8000"
+/// * Иначе используется "0.0.0.0:8000"
+pub async fn connect(db: &Surreal<Client>) -> surrealdb::Result<()> {
+    // Проверяем наличие и значение переменной окружения
+    let is_docker = env::var("IS_DOCKER")
+        .map(|val| val == "true" || val == "1")
+        .unwrap_or(false);
+
+    let address = if is_docker {
+        "surrealdb:8000"
+    } else {
+        "0.0.0.0:8000"
+    };
+
+    db.connect::<Ws>(address).await?;
+
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
+
+    db.use_ns("kpo").use_db("kpo").await?;
+
+    Ok(())
 }
