f7156a8b487679392a1726d2ffe7ddc8e8826de7
Александр Сергиенко <s_sandr06@mail.ru>
2025-12-25 20:59:23 +0300

    Обновление событий

--- crates/common/src/events.rs
+++ crates/common/src/events.rs
@@ -1,17 +1,20 @@
 use serde::{Deserialize, Serialize};
 
-/// Событие: Заказ создан. Отправляет Orders -> Payments
+/// Событие: Заказ создан.
+/// Отправляет Orders -> Payments
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OrderCreatedEvent {
-    pub order_id: String, // ID заказа
-    pub user_id: String,  // ID пользователя
-    pub total_price: f64, // Сумма к оплате
+    pub order_id: String,
+    pub user_id: String, // ID счета/пользователя
+    pub amount: f64,     // Сумма к списанию
 }
 
-/// Событие: Результат оплаты. Отправляет Payments -> Orders
+/// Событие: Результат оплаты.
+/// Отправляет Payments -> Orders
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PaymentProcessedEvent {
     pub order_id: String,
     pub success: bool,
+    // Если success = false, здесь может быть причина (например, "Insufficient funds")
     pub reason: Option<String>,
 }
